6905d2337bec0aaa5cbce255941cd4c878071425
asma <asmatra149@gmail.com>
2023-04-14 20:48:01 +0100

    addProjectController

---
+++ src/main/java/com/esprit/examen/controllers/ProjectController.java
@@ -0,0 +1,11 @@
+package com.esprit.examen.controllers;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+
+import io.swagger.annotations.Api;
+@RestController
+@CrossOrigin("*")
+@RequiredArgsConstructor
+public class ProjectController {
+}
